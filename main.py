5cea788d4c1aa40ee5cc9a942547a664f796fc84
Manuel Werder <manuelwerder@hotmail.com>
2026-01-13 13:36:16 +0100

    Adding a verbose flag

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", "-v", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 
 load_dotenv()
@@ -12,16 +14,16 @@ api_key = os.environ.get("GEMINI_API_KEY")
 if api_key is None:
     raise Exception("invalid api key")
 
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 client = genai.Client(api_key=api_key)
 
-req = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents=args.user_prompt,
-)
+req = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
 usage_metadata = req.usage_metadata
 if usage_metadata is None:
     raise RuntimeError("invalid usage metadata")
 
-print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
-print(f"Response tokens: {usage_metadata.candidates_token_count}")
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {usage_metadata.candidates_token_count}")
 print(f"Response: {req.text}")
