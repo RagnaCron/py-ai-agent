fb7b827af794fe6caaf1bfeab211ccf8f1da183f
Manuel Werder <manuelwerder@hotmail.com>
2026-01-13 13:25:10 +0100

    using argparse

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -11,8 +16,8 @@ client = genai.Client(api_key=api_key)
 
 req = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-
+    contents=args.user_prompt,
+)
 usage_metadata = req.usage_metadata
 if usage_metadata is None:
     raise RuntimeError("invalid usage metadata")
