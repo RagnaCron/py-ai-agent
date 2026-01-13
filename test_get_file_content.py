e8ef61a32ee9e3261ac8fd1ac557f00e48ea06fb
Manuel Werder <manuelwerder@hotmail.com>
2026-01-13 14:30:48 +0100

    reading out content of file and truncating file read at 10'000 chars

---
+++ test_get_file_content.py
@@ -0,0 +1,15 @@
+from functions.get_file_content import get_file_content
+
+
+content = get_file_content("calculator", "lorem.txt")
+if len(content) > 10000:
+    print(f"{content[10000:]}")
+
+content = get_file_content("calculator", "main.py")
+print(content)
+content = get_file_content("calculator", "pkg/calculator.py")
+print(content)
+content = get_file_content("calculator", "/bin/cat")
+print(content)
+content = get_file_content("calculator", "pkg/does_not_exist.py")
+print(content)
